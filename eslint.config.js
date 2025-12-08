f48f4ceb1391d24a235dc298c941a43ff83a8ff9
Alexandra <sasha.hurbnva@gmail.com>
2025-12-08 16:03:40 +0300

    test: slider component

--- eslint.config.js
+++ eslint.config.js
@@ -35,7 +35,7 @@ module.exports = defineConfig([
       ],
       '@angular-eslint/no-empty-lifecycle-method': 'warn',
       '@angular-eslint/prefer-on-push-component-change-detection': 'warn',
-      //'@angular-eslint/prefer-output-readonly': 'warn',
+      '@angular-eslint/prefer-output-readonly': 'warn',
       '@angular-eslint/prefer-signals': 'warn',
       '@angular-eslint/prefer-standalone': 'warn',
 
