0c0c95938ee7657e2fca0ffc21b2da3f9c70c17b
Sasha <sasha.hurbnva@gmail.com>
2025-12-10 17:18:27 +0300

    feat: setup routing, add 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { NotFound } from './pages/not-found/not-found';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+    title: 'Home Page',
+  },
+  {
+    path: '**',
+    component: NotFound,
+    title: '404 Page',
+  },
+];
