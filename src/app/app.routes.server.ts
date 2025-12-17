9319d34f7cc4034011b955cf594eef34e9f039f4
Sasha <sasha.hurbnva@gmail.com>
2025-12-17 15:49:28 +0300

    fix: change render mode for the Detailed Page

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'pricing/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender,
   },
