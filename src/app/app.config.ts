bf7d586b0be7a3366f260fb76cbf810b41f23ba0
Sasha <sasha.hurbnva@gmail.com>
2025-12-22 16:17:02 +0300

    feat: add image loading spinner

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,16 +6,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { cacheInterceptor } from './shared/interceptors/cache-interceptor/cache-interceptor';
 import { errorHandlingInterceptor } from './shared/interceptors/error-handling-interceptor/error-handling-interceptor';
-import { loadingStateInterceptor } from './shared/interceptors/loading-state-interceptor/loading-state-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideHttpClient(
-      withFetch(),
-      withInterceptors([cacheInterceptor, errorHandlingInterceptor, loadingStateInterceptor]),
-    ),
+    provideHttpClient(withFetch(), withInterceptors([cacheInterceptor, errorHandlingInterceptor])),
   ],
 };
