c5a3bda7e092bcc0e037a3af3416d1b79b554203
Sasha <sasha.hurbnva@gmail.com>
2025-12-24 11:38:00 +0300

    feat: add tests for Form component

--- src/app/shared/components/form/form.spec.ts
+++ src/app/shared/components/form/form.spec.ts
@@ -19,4 +19,30 @@ describe('Form', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('calls buttonHandler when submit', () => {
+    const mockButtonHandler = vi.spyOn(component, 'buttonHandler');
+
+    fixture.detectChanges();
+
+    const button = fixture.nativeElement.querySelector('button');
+
+    expect(button).toBeDefined();
+
+    button.click();
+
+    expect(mockButtonHandler).toHaveBeenCalled();
+  });
+
+  it('method getErrorMessage works correctly', () => {
+    component.errorMessages = {
+      email: 'Invalid email address',
+      minlength: (): string => 'minLength',
+    };
+    const message = component.getErrorMessage('email', 'email', 'email');
+    const message2 = component.getErrorMessage('password', 'minlength', '123');
+
+    expect(message).toEqual('Invalid email address');
+    expect(message2).toEqual('minLength');
+  });
 });
