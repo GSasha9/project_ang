6aba52486ce333214d441b1f26139e9347519635
Sasha <sasha.hurbnva@gmail.com>
2026-01-13 15:55:26 +0300

    refactor: replace custom slider with Bootstrap carousel

--- src/app/layout/slides/lesson-2/lesson-2.ts
+++ src/app/layout/slides/lesson-2/lesson-2.ts
@@ -1,9 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { Button } from '@shared/components/button/button';
+import { Link } from '@shared/components/link/link';
 
 @Component({
   selector: 'app-lesson-2',
-  imports: [Button],
+  imports: [Link],
   templateUrl: './lesson-2.html',
   styleUrl: './lesson-2.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
