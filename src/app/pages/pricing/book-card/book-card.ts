a0724a0fd019980c032c933780ea4b80e8d37b50
Sasha <sasha.hurbnva@gmail.com>
2026-01-13 14:39:54 +0300

    feat: add images to cards

--- src/app/pages/pricing/book-card/book-card.ts
+++ src/app/pages/pricing/book-card/book-card.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, signal } from '@angular/core';
+import { Spinner } from '@shared/components/spinner/spinner';
 import { Book } from '@shared/models/book.model';
 
 @Component({
@@ -6,14 +7,20 @@ import { Book } from '@shared/models/book.model';
   templateUrl: './book-card.html',
   styleUrl: './book-card.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [Spinner],
 })
 export class BookCard {
   readonly bookData = input<Book>();
   readonly handler = input<(id: number) => void>();
   readonly buttonHandler = input<(event: MouseEvent) => void>();
   readonly isSelected = input<boolean | undefined>(undefined);
+  readonly imageLoad = signal(true);
 
   cardHandler = (): void => {
     this.handler()?.(this.bookData()?.id || 1);
+  };
+
+  onImgLoad = (): void => {
+    this.imageLoad.set(false);
   };
 }
